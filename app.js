2988a79f303ec64de3de77830c90316939d30718
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-09-19 18:15:04 -0700

    final JS update before canvas submission

--- app.js
+++ app.js
@@ -9,19 +9,19 @@
 var userName = prompt('Arrrrr, hallo thar! Tis talk like a pirate dayyyyyy! What be yer name!?');
 
 alert('Ahoy, ' + userName + ', I can see ye be a landlubber of the wretchedest sort. See if ye can answer me riddles about my life pirating on the high seas!');
-console.log('userName value is' + userName);
+console.log('userName value is', userName);
 
 var answer1 = prompt('Do I pillage according to a code of conduct!?').toUpperCase();
-console.log('answer1 value is' + answer1);
+console.log('answer1 value is', answer1);
 
-if (answer1 === 'N' || answer1 === 'NO'  || answer1 === 'NAY') {
+if (answer1 === 'N' || answer1 === 'NO' || answer1 === 'NAY') {
   alert('Haaar! Ye be wise - we pillage as we please!');
 } else {
   alert('Ah ha ha ha, nay, you may wish it true to save yer hide, but our morals are as thin as our daily grog!');
 }
 
 var answer2 = prompt('Does my ship o\' thee damned have a mascot?').toUpperCase();
-console.log('answer2 value is' + answer2);
+console.log('answer2 value is', answer2);
 
 
 if (answer2 === 'Y' || answer2 === 'YES' || answer2 === 'YAR') {
@@ -31,7 +31,7 @@ if (answer2 === 'Y' || answer2 === 'YES' || answer2 === 'YAR') {
 }
 
 var answer3 = prompt('Arrr, am I havin\' more rum?').toUpperCase();
-console.log('answer3 value is' + answer3);
+console.log('answer3 value is', answer3);
 
 if (answer3 === 'Y' || answer3 === 'YES' || answer3 === 'YAR') {
   alert('HOW COULD THERE BE ANY OTHER ANSWER, ME MATEY?');
@@ -40,16 +40,16 @@ if (answer3 === 'Y' || answer3 === 'YES' || answer3 === 'YAR') {
 }
 
 var answer4 = prompt('Can this pirate code?').toUpperCase();
-console.log('answer4 value is' + answer4);
+console.log('answer4 value is', answer4);
 
 if (answer4 === 'Y' || answer4 === 'YES' || answer4 === 'YAR') {
-  alert('Tis true! Our servers are colocated in Tortuga');
+  alert('Tis true! Our servers are colocated in Tortuga.');
 } else {
   alert('Haaar, have ye so little faith? I may make me mark wit\' an X, but I type by touch!');
 }
 
 var answer5 = prompt('Arrr, do I treat every day as Talk Like A Pirate Day, whether th\' season be foul or fair?').toUpperCase();
-console.log('answer5 value is' + answer5);
+console.log('answer5 value is', answer5);
 
 if (answer5 === 'N' || answer5 === 'NO' || answer5 === 'NAY') {
   alert('You\'re right! Thank goodness, that would be exhausting for all involved');
